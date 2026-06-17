e09d5f6028cebc064c2c0057e6131828287f6da0
Radhika Sutar <radhikasutar14@gmail.com>
2026-06-17 16:34:01 +0530

    Add Docker and Docker Compose configuration

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,6 @@ function App(){
     <>
     <Navbar/>
     <AppRoutes />
-    {/* <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/register" element={<Register/>} />
-      <Route path="/applications/:jobId" element={<JobApplication />}/>
-      <Route path="/candidate/dashboard" element={
-          <ProtectedRoute role="candidate">
-            <CandidateDashboard/>
-          </ProtectedRoute>
-        } 
-        />
-      <Route path="/recruiter/dashboard" element={
-          <ProtectedRoute role="recruiter">
-            <RecruiterDashboard/>
-          </ProtectedRoute>
-        } />
-      <Route path="/post-job" element={<PostJob />} />
-      <Route path="/edit-job/:jobId" element={<EditJob />} />
-      <Route  path="/saved-jobs" element={<SavedJob />}/>
-      <Route path="/upload-resume" element={<ResumeUpload />}/>
-    </Routes> */}
-    
     </>
   )
 }
